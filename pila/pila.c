6d3abac1191a678dcd8a6cb31540ad7c0094a127
masacr3 <leorhcp2030@gmail.com>
2018-04-13 14:18:50 -0300

    Remove constantes , redimensionar, printf, add volumen

--- pila/pila.c
+++ pila/pila.c
@@ -12,18 +12,15 @@
 #include "pila.h"
 #include <stdlib.h>
 
-#define PILA_VACIA 0
-#define ARREGLO 10
+#define TAM_INICIAL_ARREGLO 10
 #define AGRANDAR_TAMANIO 2
-#define ERROR_MALLOC NULL;
-#define ERROR_REDIMENSIONAR false
 
 /* Definición del struct pila proporcionado por la cátedra.
  */
 struct pila {
     void** datos;
     size_t cantidad;  // Cantidad de elementos almacenados.
-    size_t capacidad;  // Capacidad del arreglo 'datos'.
+    size_t capacidad;  // Capacidad del ARREGLO 'datos'.
 };
 
 /* *****************************************************************
@@ -32,22 +29,19 @@ struct pila {
 pila_t* pila_crear(void){
   pila_t * pila = malloc( sizeof(pila_t) );
 
-  if (!pila){
-    return ERROR_MALLOC;
-  }
+  if (!pila) return NULL;
 
-  pila->datos = malloc (sizeof(void*) * ARREGLO);
+  pila->datos = malloc (sizeof(void*) * TAM_INICIAL_ARREGLO);
 
   if (!pila->datos){
-    pila_destruir(pila);
-    return ERROR_MALLOC;
+    free(pila);
+    return NULL;
   }
 
-  pila->cantidad = PILA_VACIA;
-  pila->capacidad = ARREGLO;
+  pila->cantidad = 0;
+  pila->capacidad = TAM_INICIAL_ARREGLO;
 
   return pila;
-
 }
 
 void pila_destruir(pila_t *pila){
@@ -56,15 +50,13 @@ void pila_destruir(pila_t *pila){
 }
 
 bool pila_esta_vacia(const pila_t *pila){
-  return pila->cantidad == PILA_VACIA;
+  return pila->cantidad == 0;
 }
 
 bool redimensionar_pila(pila_t* pila, size_t tamanio){
   void **datos_redimencionados = realloc(pila->datos, tamanio * sizeof(void *));
 
-  if (!datos_redimencionados){
-    return ERROR_REDIMENSIONAR;
-  }
+  if (!datos_redimencionados) return false;
 
   pila->datos = datos_redimencionados;
   pila->capacidad = tamanio;
@@ -77,9 +69,7 @@ bool pila_apilar(pila_t *pila, void* valor){
   if (aumentar_tamanio_ok){
     bool ok = redimensionar_pila(pila,pila->capacidad * AGRANDAR_TAMANIO);
 
-    if (!ok){
-      return ERROR_REDIMENSIONAR;
-    }
+    if (!ok) return NULL;
   }
 
   pila->datos[pila->cantidad++] = valor;
@@ -91,22 +81,10 @@ void* pila_ver_tope(const pila_t *pila){
 }
 
 void* pila_desapilar(pila_t *pila){
-  if (pila_esta_vacia(pila)){
-    return NULL;
-  }
+  if (pila_esta_vacia(pila)) return NULL;
 
-  bool reducir_tamanio_ok = (pila->cantidad * 4) <= pila->capacidad && (pila->capacidad / AGRANDAR_TAMANIO) >= ARREGLO ;
-
-  if (reducir_tamanio_ok ){
-    bool ok = redimensionar_pila(pila,pila->capacidad / AGRANDAR_TAMANIO);
-
-    if (!ok){
-      return ERROR_REDIMENSIONAR;
-    }
-  }
+  bool reducir_tamanio_ok = (pila->cantidad * 4) <= pila->capacidad && (pila->capacidad / AGRANDAR_TAMANIO) >= TAM_INICIAL_ARREGLO ;
+  if (reducir_tamanio_ok ) redimensionar_pila(pila,pila->capacidad / AGRANDAR_TAMANIO);
 
   return pila->datos[--pila->cantidad] ;
 }
-
-
-// ...
--- pila/pruebas_alumno.c
+++ pila/pruebas_alumno.c
@@ -17,9 +17,7 @@ void pruebas_pila_vacia(){
   print_test("La pila esta creada", pila != NULL);
   print_test("La pila esta vacia", pila_esta_vacia(pila) );
 
-  //caso borde
   print_test("Ver tope es una operacion invalida", !pila_ver_tope(pila));
-  //caso borde
   print_test("Desapilar es una operacion invalida", !pila_desapilar(pila));
 
   pila_destruir(pila);
@@ -47,54 +45,50 @@ void pruebas_pila_apilar(){
 
 void pruebas_pila_desapilar(){
   printf("\n\nPRUEBAS DESAPILAR\n");
+
   pila_t* pila = pila_crear();
   int array[] = {1,2,3};
+
   print_test("La pila esta creada", pila != NULL);
   print_test("La pila esta vacia", pila_esta_vacia(pila) );
 
-  for (int i = 0; i < 3; i++){
-    pila_apilar(pila,&array[i]);
-  }
-  print_test("Pila cargada con elementos -> [1,2,3]",!pila_esta_vacia(pila));
+  bool apilar_elementos_ok = true;
+  for (int i = 0; i < 3; i++) apilar_elementos_ok &= pila_apilar(pila,&array[i]);
 
+  print_test("Pila cargada con elementos -> [1,2,3]", apilar_elementos_ok);
   print_test("Desapilar 3", *(int*)pila_desapilar(pila) == 3);
   print_test("Desapilar 2", *(int*)pila_desapilar(pila) == 2);
   print_test("Desapilar 1", *(int*)pila_desapilar(pila) == 1);
   print_test("La pila esta vacia no se puede desapilar", !pila_desapilar(pila) );
-
   pila_destruir(pila);
   print_test("pila destruida",true);
-
 }
 
-void prueba_pila_redimensionar(){
-  printf("\n\nPRUEBAS REDIMENSIONAR\n");
+
+void pruebas_pila_volumen(){
+  printf("\n\nPRUEBAS DE VOLUMEN PILA\n");
+
   pila_t* pila = pila_crear();
-  int array[] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
   print_test("La pila esta creada", pila != NULL);
   print_test("La pila esta vacia", pila_esta_vacia(pila) );
 
-  for (int i=0; i<10; i++){
-    pila_apilar(pila,&array[i]);
-  }
-  print_test("La pila esta llenada hasta su capacidad maxima = 10",true);
-  print_test("redimensiono y apilo 11", pila_apilar(pila,&array[10]));
-  print_test("chequeo el tope tiene que ser 11", *(int*)pila_ver_tope(pila) == 11);
-  print_test("apilo 12", pila_apilar(pila,&array[11]));
-  print_test("chequeo el tope tiene que ser 12", *(int*)pila_ver_tope(pila) == 12);
-  print_test("apilo 13", pila_apilar(pila,&array[12]));
-  print_test("chequeo el tope tiene que ser 13", *(int*)pila_ver_tope(pila) == 13);
+  size_t TAM = 5000;
+  int array[TAM];
 
-  print_test("Empiezo a desapilar [1,2,3,4,5,6,7,8,9,10,11,12,13]",true);
-  for (int i=0; i<13 ; i++)
-  {
-    printf("desapilando %d",13-i);
-    print_test(" ",*(int*) pila_desapilar(pila) == array[12-i]);
-  }
+  for (int i =0 ; i< TAM ; i++) array[i] = i;
+
+  bool apilar_elementos_ok = true;
+  for (int i =0 ; i <TAM ; i++) apilar_elementos_ok &= pila_apilar(pila,&array[i]) && *(int*)pila_ver_tope(pila) == array[i];
+
+  print_test("Se apilaron todos los elementos correctamente",apilar_elementos_ok);
+
+  bool desapilar_elementos_ok = true;
+  for(int i =0; i<TAM ; i++) desapilar_elementos_ok &= *(int*)pila_desapilar(pila) == array[TAM-1-i];
+
+  print_test("Se desapilaron todos los elementos correctamente",desapilar_elementos_ok);
   print_test("La pila tiene que estar vacia", pila_esta_vacia(pila));
   pila_destruir(pila);
   print_test("pila destruida",true);
-
 }
 
 
@@ -102,5 +96,5 @@ void pruebas_pila_alumno() {
   pruebas_pila_vacia();
   pruebas_pila_apilar();
   pruebas_pila_desapilar();
-  prueba_pila_redimensionar();
+  pruebas_pila_volumen();
 }
