f9e44d4f1d6c7fbe838951a08bf747e1e510ca8e
Leonel R <leorhcp2030@gmail.com>
2018-04-17 11:11:03 -0300

    add iter

--- listas/lista.h
+++ listas/lista.h
@@ -9,3 +9,13 @@ void *lista_ver_primero(const lista_t *lista);
 void *lista_ver_ultimo(const lista_t* lista);
 size_t lista_largo(const lista_t *lista);
 void lista_destruir(lista_t *lista, void destruir_dato(void *));
+
+typedef struct lista_iter lista_iter_t;
+
+lista_iter_t *lista_iter_crear(lista_t *lista);
+bool lista_iter_avanzar(lista_iter_t *iter);
+void *lista_iter_ver_actual(const lista_iter_t *iter);
+bool lista_iter_al_final(const lista_iter_t *iter);
+void lista_iter_destruir(lista_iter_t *iter);
+bool lista_iter_insertar(lista_iter_t *iter, void *dato);
+void *lista_iter_borrar(lista_iter_t *iter);
