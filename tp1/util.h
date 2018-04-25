955be4878b412f8ac57ff0db624cf6bb12561316
Leonel R <leorhcp2030@gmail.com>
2018-04-25 11:17:06 -0300

    Add files via upload

---
+++ tp1/util.h
@@ -0,0 +1,17 @@
+/*
+ * Leonel R.
+ */
+
+#include <stdlib.h>
+
+char* slice(const char* str, size_t inicio, size_t fin);
+
+/*
+ * Devuelve las posiciones donde esta SEP
+ * posiciones[0] guarda el tamaño del array
+ * posiciones[>0] se guardan las posiciones relativas de SEP
+ * Post:
+ *   >   si no hay coincidencias posiciones[0] = 0
+ *   >   si hay coincidencias posiciones[0] = coincidencias , len(posiciones) = coincidencias
+ */
+int* buscar_sep(const char* str, char* sep);
