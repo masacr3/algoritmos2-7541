916d47427848505bd208f7fdc457b8f4d1eede21
martinstd96 <38985104+martinstd96@users.noreply.github.com>
2018-05-04 15:28:33 -0300

    add contenido de la funcion

--- hash/hash_crear.c
+++ hash/hash_crear.c
@@ -1,1 +1,13 @@
-hash_t *hash_crear(hash_destruir_dato_t destruir_dato);
+hash_t* hash_creaar(hash_destruir_dato_t destruir_dato){
+  hash_t* hash=malloc(sizeof(hash_t));
+  if (!hash) return NULL;
+  hash->tabla=malloc(sizeof(lista_t)*TAMANIO);
+  if (!hash->tabla){
+    free(hash);
+    return NULL;
+  }
+  hash->capacidad=TAMANIO;
+  hash->cantidad=CANTIDAD_INICIAL;
+  hash->dato=destruir_dato;
+  return hash;
+}
