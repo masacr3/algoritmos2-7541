7cea56747e482586ee010581a3e98d0e1e97927b
masacr3 <leorhcp2030@gmail.com>
2018-05-04 15:27:04 -0300

    marto ahi te subi hash_crear

--- hash/hash_crear.c
+++ hash/hash_crear.c
@@ -1,1 +1,24 @@
-hash_t *hash_crear(hash_destruir_dato_t destruir_dato);
+hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
+  hash_t* hash = malloc(sizeof(hash_t));
+
+  if(!hash) return NULL;
+
+  //tenemos que tener un tamanio inicial
+
+  lista_t** tabla = (lista_t**) malloc(sizeof(lista_t*) * TAM_INICIAL);
+
+  if(!tabla){
+    free(hash);
+    return NULL;
+  }
+
+  hash->tabla = tabla;
+
+  for(int i=0 ; i < TAM_INICIAL ; i++) hash->tabla[i] = lista_crear();
+
+  hash->capacidad = TAM_INICIAL;
+  hash->cantidad = 0;
+  hash->destructor = destruir_dato;
+
+  return hash;
+}
